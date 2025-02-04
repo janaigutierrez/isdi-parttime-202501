8983fe5ef0b50ab2d487b21d21ee394753f21d5d
unknown <janaigs97@gmail.com>
2025-02-04 18:01:18 +0100

    added more notes

--- staff/janai-gutierrez/javascript/dataTypes.js
+++ staff/janai-gutierrez/javascript/dataTypes.js
@@ -7,3 +7,47 @@ console.log(str2.lenght);
 var num = 1;
 var numInt = 4567;
 var numFloat = 0.58;
+
+console.log(num + numInt);
+
+var arrr = ['a', 12, Array [3, 2, 3], '12'];
+
+arrr [0] 
+
+arrr [2][1] 
+
+
+for(var i = 0; i > arrr.lenght; i++) {
+    if(arrr[i].lenght) {
+        alert(arrr[i])
+    }
+}
+
+var truth = true
+var truth2 = 'true'
+
+truth === truth 
+
+truth !== truth2
+
+!true === false  //devolvería true
+true !==  false  //devolvería true
+true === false  //devolvería false
+
+if(true === !false) {
+    console.log("Hola mundo")
+}
+var arr = [1, 2, 3, 4];
+
+var i = 0
+while(i < arr.lenght) {
+
+}
+    if(i < arr.lenght / 2) {
+
+    
+    alert(arr[i]);
+    i++
+}
+
+
