8091dead3be0290c3bf6158e2bf3360d925bf2b3
unknown <janaigs97@gmail.com>
2025-02-04 17:47:22 +0100

    add number game2.0 needs fixing

--- staff/janai-gutierrez/javascript/numberGuess.js
+++ staff/janai-gutierrez/javascript/numberGuess.js
@@ -1,15 +1,32 @@
-function guessNumber() {
-    var number = 8;
-    var guess = prompt ('qué número estoy pensando?');
-    if (number === guess) {
-        alert('Bingo!')
+var isGameOn = confirm('Wanna play a game?');
+if(isGameOn) {
+    numberGuessGame()
+} else {
+    alert('Ciao!');
+}
+function numberGuessGame() {
+    var winNumber = Math.floor(Math.random()* 10 + 1);
+    var attempts = 0
+    var guess = null
+    
+
+    while(guess !== winNumber) {
+        var input = prompt('Which one is the secret number?');
+        if(input === null) {
+            alert('Leaving the game');
+            return;
+        } 
+    }
+    var guess = parseInt(input);
+    attempts++;
+
+    if(isNaN(guess)) {
+        alert("That's not a number sir.")
+    } else if(guess < winNumber) {
+        alert('Too low, try again');
+    } else if(guess > winNumber) {
+        alert("Too high, try again");
     } else {
-        alert('Fatal');
-        if((guess - number) > 5) {
-            console.log("Frío")
-        }
-        else {
-            console.log("Caliente");
-        }
+        alert("Congrats! You solved the game in ${attempts} attempts.")
     }
 }
