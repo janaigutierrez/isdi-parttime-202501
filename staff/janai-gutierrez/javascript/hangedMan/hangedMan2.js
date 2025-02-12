55f6d4d97c3e2193ee953da82cd36ffce7d85092
unknown <janaigs97@gmail.com>
2025-02-13 00:21:48 +0100

    new render to be fixed #23

--- staff/janai-gutierrez/javascript/hangedMan/hangedMan2.js
+++ staff/janai-gutierrez/javascript/hangedMan/hangedMan2.js
@@ -22,7 +22,7 @@ return false;
 function hiddenWord(length) {
     var hidden = '';
     for (var i = 0; i < length; i++) {
-                   hidden += '-';
+            hidden += '-';
         }
     
     return hidden;
@@ -55,6 +55,119 @@ function toLowerCase(letter) {
     }
     return letter;
 }
+
+//starting render
+var body = document.body;
+var wordContainer;
+var livesContainer;
+var letterFormContainer;
+
+//body area
+body.style.display = 'flex';
+body.style.flexDirection = 'column';
+body.style.gap = '2rem2';
+
+//title in body area
+var gameTitle = document.createElement('h1');
+gameTitle.textContent = 'THE HANGMAN GAME';
+gameTitle.style.textAlign = 'center';
+
+body.appendChild(gameTitle);
+
+
+function renderInterface() {
+    wordContainer = document.createElement('div');
+    wordContainer.style.width = '100%';
+    wordContainer.style.display = 'flex';
+    wordContainer.style.flexDirection = 'row';
+    wordContainer.style.gap = '0.5rem';
+    wordContainer.style.justifyContent = 'center';
+
+    for (var i = 0; i <guessedWord.length; i++) {
+        var letterSquare = document.createElement('div');
+        letterSquare.style.height = "2rem";
+        letterSquare.style.width = "2rem";
+        letterSquare.style.border = "2px dashed slategray"
+        letterSquare.style.display = "flex";
+        letterSquare.style.justifyContent = "center";
+        letterSquare.style.alignItems = "center";
+        if (guessedWord[i] !== '-') {
+            var letterContainer = document.createElement('b');
+            letterContainer.textContent = guessedWord[i].toUpperCase();
+            letterSquare.style.border = '2px solid green'
+            letterSquare.style.backgroundColor = 'fuchsia'
+            letterSquare.appendChild(letterContainer)
+
+        }
+     wordContainer.appendChild(letterSquare)
+       
+    }
+
+    livesContainer = document.createElement('div');
+
+    livesContainer.style.width = '100%';
+    livesContainer.style.display = 'flex';
+    livesContainer.style.flexDirection = 'row';
+    livesContainer.style.gap = '1rem';
+    livesContainer.style.justifyContent = 'center';
+
+    for (var i = 0; i < lives; i++) {
+        var lifeIcon = document.createElement('div');
+        liveIcon.style.width = '1rem';
+        liveIcon.style.height = '1rem';
+        liveIcon.style.borderRadius = '50%';
+        liveIcon.style.backgroundColor = 'red'
+
+        livesContainer.appendChild(lifeIcon)
+    }
+
+    letterFormContainer = document.createElement('form');
+
+    letterFormContainer.style.display = 'flex';
+    letterFormContainer.style.flexDirection = 'row';
+    letterFormContainer.style.width = '100%';
+    letterFormContainer.style.gap = '0.5rem';
+    letterFormContainer.style.justifyContent = 'center';
+
+    var letterInput = document.createElement('input');
+    letterInput.type = 'text';
+    letterInput.minLength = 1;
+    letterInput.maxLength = 1;
+    letterInput.required = true;
+    letterInput.id = 'letter';
+    letterInput.style.width = '2rem'
+
+    var submitButton = document.createElement('input');
+    submitButton.type = 'submit';
+
+    letterFormContainer.appendChild(letterInput);
+    letterFormContainer.appendChild(submitButton);
+
+    body.appendChild(wordContainer)
+    body.appendChild(lifesContainer)
+    body.appendChild(letterFormContainer)
+}
+
+function cleanInterface() {
+    body.removeChild(wordContainer);
+    body.removeChild(lifesContainer)
+    body.removeChild(letterFormContainer)
+    wordContainer = undefined;
+    lifesContainer = undefined;
+    letterFormContainer = undefined;
+}
+
+
+
+renderInterface()
+
+addEventListener('submit', function (event) {
+    event.preventDefault();
+    var letterValue = event.target.letter.value;
+    playGame(letterValue)
+    //event.stopImmediatePropagation() --> sirve para no llamar al mismo tipo de evento varias veces
+})
+
 
 //main game code
 
