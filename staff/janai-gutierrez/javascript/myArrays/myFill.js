13f6938527513a90cc9eeea815c61c1a858ff2cb
unknown <janaigs97@gmail.com>
2025-03-02 23:12:55 +0100

    myFill done #23

--- staff/janai-gutierrez/javascript/myArrays/myFill.js
+++ staff/janai-gutierrez/javascript/myArrays/myFill.js
@@ -4,11 +4,16 @@ var controlArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 var testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-console.log(controlArray.fill(0, 5))
+console.log(controlArray.fill(0, 5, 9));
 
+function myFill(array, num, [i], [j]) {
+    for(var l = i; l <= j; i++)
+        array[l] = num
+    return array;
+};
 
+console.log(myFill(testArray, 0, [5], [9]));
 
+console.log('testing');
 
-
-
-console.assert(controlArray.fill(0, 5) === myFill, `Error: the returned arrays do not coincide`);
+console.assert(controlArray.fill(0, 5, 9) === myFill, `Error: the returned arrays do not coincide`);
---
+++ staff/janai-gutierrez/javascript/myArrays/myFindIndex.js
@@ -0,0 +1,20 @@
+//returns the index of the first ekement in the array that satisfies the provided testing function
+
+var testArray = [4, 13, 8, 55, 200, 22];
+var controlArray = [4, 13, 8, 55, 200, 22];
+
+var controlElement = (element) => element > 13;
+
+console.log(controlArray.findIndex(controlElement));
+
+//expected output: 3
+
+function myFillIndex(){
+
+
+}
+
+//testing
+console.log(testing);
+
+console.assert();
