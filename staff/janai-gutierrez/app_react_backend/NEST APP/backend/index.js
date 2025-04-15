c392dc4416fead039bb42b2970fc67c9350b009a
unknown <janaigs97@gmail.com>
2025-04-15 20:49:31 +0200

    moved validators and errors to common #32

--- staff/janai-gutierrez/app_react_backend/NEST APP/backend/index.js
+++ staff/janai-gutierrez/app_react_backend/NEST APP/backend/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { data } from 'data';
+import { data } from './data/index.js';
 import { json } from 'express';
- 
+import { errors } from 'common';
+
 const api = express()
 
 const jsonBodyParser = json()
@@ -19,23 +20,39 @@ api.get('/api', (req, res) => {
 api.post('/user', jsonBodyParser, (req, res) => {
     const { email, password } = req.body
 
-    try {
+    const username = email.split('@')[0]
+
+    const saveUser = (error) => {
+
+    }
+
+    /* try {
         const findUser = data.users.findUserByEmail(email, console.error)
 
-    if(findUser) {
-        console.error('user already exists')
-        res.status(409)
-        res.send('user already exists')
-    }
-    data.users.createUser({email, password}, console.error)
+        if(findUser) {
+            throw new errors.DuplicityError('user already exists')
+            
+    } */
 
+    data.users.createUser({email, password, username}, (error, user) => {
+        if (error) res.status(500).send(error.message)
+            if (user) res.status(201).send()
+                res.status(500).send('something went wrong')
+    })
     res.status(201).send()
 
-    } catch (error) {
+    /* } catch (error) {
+        if(error instanceof ExistenceError)
+            res.status(409).send('user already exists')
         res.status(500).send()
-    }
+    } */
 
     
+})
+
+api.put('/users', (req, res) => {
+    res.status(200)
+    res.send('HelloPutUsers!')
 })
 
 api.listen(port, () => {
--- staff/janai-gutierrez/app_react_backend/NEST APP/common/errors.js
+++ staff/janai-gutierrez/app_react_backend/NEST APP/common/errors.js
@@ -1,27 +1,34 @@
 export class FormatError extends Error {
     constructor(message) {
         super(message)
-        this.name = 'FormatError'
+        this.name = FormatError
     }
 }
 
 export class AuthError extends Error {
     constructor(message) {
         super(message)
-        this.name = 'AuthError'
+        this.name = AuthError
     }
 }
 
 export class ExistenceError extends Error {
     constructor(message) {
         super(message)
-        this.name = 'ExistenceError'
+        this.name = ExistenceError
     }
 }
 
 export class ContentError extends Error {
     constructor(message) {
         super(message)
-        this.name = 'ContentError'
+        this.name = ContentError
+    }
+}
+
+export class DuplicityError extends Error {
+    constructor(message){
+        super(message)
+        this.name = DuplicityError
     }
 }
