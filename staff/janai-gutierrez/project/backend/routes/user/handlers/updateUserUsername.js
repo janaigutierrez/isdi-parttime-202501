0f59170c7d2bd6627ec0472de246dd4c13082caa
unknown <janaigs97@gmail.com>
2025-07-08 19:55:44 +0200

    fix id/_id issue #51

--- staff/janai-gutierrez/project/backend/routes/user/handlers/updateUserUsername.js
+++ staff/janai-gutierrez/project/backend/routes/user/handlers/updateUserUsername.js
@@ -4,11 +4,11 @@ import logic from "../../../logics/index.js"
 const updateUserUsername = async (req, res, next) => {
     try {
         const userId = req.user.id
-        const { newUsername } = req.body
+        const { username } = req.body
 
-        validator.username(newUsername)
+        validator.username(username)
 
-        const result = await logic.updateUserUsername(userId, newUsername)
+        const result = await logic.updateUserUsername(userId, username)
 
         res.status(200).json({
             success: true,
